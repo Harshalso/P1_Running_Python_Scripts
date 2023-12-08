6056ed418da828a031b415fbacdff53a2e462ffa
Harshalso <harshalsonawane95@gmail.com>
2023-12-08 16:04:28 +0530

    files push

--- first.py
+++ first.py
@@ -2,3 +2,18 @@ print("Hello world")
 name = "Harshal"
 surname = "Sonawane"
 print("My name is {} and surname is {}".format(name, surname))
+
+age = "28"
+print(type(age))
+print(type(surname))
+
+result_integer = int(age)
+print(type(result_integer))
+
+age1 = str(result_integer)
+print(type(age1))
+
+true_boolean = True
+false_boolean = False
+print(type(true_boolean))
+print(type(false_boolean))
